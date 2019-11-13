e65544397aa05e34f997f5cfb6dc620de0a485d9
Nilesh Patel <palnil@gmail.com>
2019-11-12 19:28:49 -0500

    "initial checking"

---
+++ src/main/java/com/yoshallc/mortgage/mortgage/App.java
@@ -0,0 +1,13 @@
+package com.yoshallc.mortgage.mortgage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
