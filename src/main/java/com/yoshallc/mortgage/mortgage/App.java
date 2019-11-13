853c5f965fc94fd1d5134fd05816dccd3a34952f
Nilesh Patel <palnil@gmail.com>
2019-11-12 19:51:58 -0500

    Adds new feature

--- src/main/java/com/yoshallc/mortgage/mortgage/App.java
+++ src/main/java/com/yoshallc/mortgage/mortgage/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "This is mortgage application" );
+        System.out.println( "would like to create account" );
     }
 }
