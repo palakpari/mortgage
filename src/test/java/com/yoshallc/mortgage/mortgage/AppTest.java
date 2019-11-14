8e32cda240b77ff26a2961ed57b812e54755853d
Nilesh Patel <palnil@gmail.com>
2019-11-13 22:20:25 -0500

    adds testng

--- src/test/java/com/yoshallc/mortgage/mortgage/AppTest.java
+++ src/test/java/com/yoshallc/mortgage/mortgage/AppTest.java
@@ -2,44 +2,17 @@ package com.yoshallc.mortgage.mortgage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-
-public class AppTest 
-    extends TestCase
-{  
+public class AppTest {
 	private final Logger logger = LoggerFactory.getLogger(AppTest.class);
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-    	System.out.println("Hello1");
-        return new TestSuite( AppTest.class );
-    }
+	@Test()
+	public void testEmailGenerator() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-    	logger.info("Hello");
-        assertTrue( true );
-    }
+		
+		Assert.assertEquals(true, 5==5);
+	}
 }
