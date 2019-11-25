f22d15a54f43062106f52ca51ef3d78c3ec4221b
palakpari <palakparipatel@gmail.com>
2019-11-24 22:49:40 -0500

    add nested json validation
    Co-Authored-By: palakpari <palakpari@users.noreply.github.com>

--- src/test/java/com/yoshallc/mortgage/AppTest.java
+++ src/test/java/com/yoshallc/mortgage/AppTest.java
@@ -1,12 +1,20 @@
 package com.yoshallc.mortgage;
 
+import groovy.util.logging.Slf4j;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class AppTest {
+
+	private static Logger logger = LoggerFactory.getLogger(AppTest.class);
 
 	@Test()
 	public void testGoogle_gives200OK() throws InterruptedException {
@@ -29,4 +37,15 @@ public class AppTest {
 		given().when().get("https://reqres.in/api/users/23").then().statusCode(404);
 
 	}
+
+	@Test
+	public void testOneUser_givenUserIsJanet(){
+
+		logger.info(given().when().get("https://reqres.in/api/users/2").body().asString());
+
+		given().when().get("https://reqres.in/api/users/2").then().
+				body("data.first_name", equalTo("Janet"));
+
+	}
+
 }
