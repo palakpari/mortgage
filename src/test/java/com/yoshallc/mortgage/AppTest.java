69c5418f907fa55e3a5896b8eab1c42aeb47be15
palakpari <palakparipatel@gmail.com>
2019-11-29 20:32:10 -0500

    reporter class
    add reporter class
    
    Co-Authored-By: palakpari <palakpari@users.noreply.github.com>

--- src/test/java/com/yoshallc/mortgage/AppTest.java
+++ src/test/java/com/yoshallc/mortgage/AppTest.java
@@ -3,6 +3,7 @@ package com.yoshallc.mortgage;
 import com.yoshallc.core.BaseTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -17,13 +18,27 @@ public class AppTest extends BaseTest {
 
 
     @Test
-    public void testItsMainPage(){
+    public void Verify_Its_BAPSSatsangExam_MainPage(){
+
 
         MainPage mainPage = new MainPage(driver);
+        Reporter.log("Loading BAPS Satsang Exam Website");
         mainPage.goToMainPage();
+        Reporter.log("Validating title of BAPS Satsang Website to \"Welcome\" ");
         assertEquals(mainPage.validateMainPageTitle(),"Welcome");
 
     }
 
+    @Test
+    public void Verify_Its_Google_MainPage(){
+
+
+        MainPage mainPage = new MainPage(driver);
+        Reporter.log("Loading BAPS Satsang Exam Website");
+        mainPage.goToMainPage();
+        Reporter.log("Validating title of BAPS Satsang Website to \"Google\" ");
+        assertEquals(mainPage.validateMainPageTitle(),"Google");
+
+    }
 
 }
